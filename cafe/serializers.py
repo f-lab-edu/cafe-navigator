8dba7b29c54411758df5fe1f439e8f05c06198d5
유수화 <dbtnghk528@gmail.com>
2023-10-09 14:16:54 +0900

    add comment and like_count to cafe serializer

--- cafe/serializers.py
+++ cafe/serializers.py
@@ -1,16 +1,8 @@
 from rest_framework import serializers
-from cafe.models import Cafe, Comment, Like
+from cafe.models import Cafe, Comment, Like 
 
 
-class CafeSerializers(serializers.ModelSerializer):
-    staff  = serializers.ReadOnlyField(source='user.username')
-    
-    class Meta:
-        model = Cafe
-        fields = '__all__'
-
-
-class CommentSerializers(serializers.ModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
     
     class Meta:
@@ -18,9 +10,26 @@ class CommentSerializers(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class LikeSerializers(serializers.ModelSerializer):
+class LikeSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
 
     class Meta:
         model = Like
         fields = '__all__'
+
+
+class CafeSerializer(serializers.ModelSerializer):
+    staff  = serializers.ReadOnlyField(source='user.username')
+    like_count = serializers.SerializerMethodField()
+    comments = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Cafe
+        fields = '__all__'
+
+    def get_like_count(self, obj):
+        return Like.objects.filter(cafe=obj).count()
+    
+    def get_comments(self, obj):
+        comments = Comment.objects.filter(cafe=obj)
+        return CommentSerializer(comments, many=True).data
