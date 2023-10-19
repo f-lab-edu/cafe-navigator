835d58431babcd73ca1362fa0be3f40aecab5044
유수화 <dbtnghk528@gmail.com>
2023-10-19 17:02:41 +0900

    commit first test

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import json
+import logging
+import statistics
 
-# Create your tests here.
+
+class RegisterAPIViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = "/user/signup"
+    
+    def test_register(self):
+        # 성공
+        response = self.client.post(
+            self.url,
+            {
+                "username": "teststaff1",
+                "password": "1234",
+                "is_staff": 1
+            },
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 201)
