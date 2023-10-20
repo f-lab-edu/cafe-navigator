b5fea1d65bde7b9d4a138ce80b676251a3e9a41c
유수화 <dbtnghk528@gmail.com>
2023-10-20 23:15:13 +0900

    add user test: signup & login

--- user/tests.py
+++ user/tests.py
@@ -1,22 +1,70 @@
-from django.test import TestCase, Client
-import json
-import logging
-import statistics
+import pytest
+from user.models import User
+from django.urls import reverse
+from rest_framework import status
 
 
-class RegisterAPIViewTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.url = "/user/signup"
-    
-    def test_register(self):
-        # 성공
-        response = self.client.post(
-            self.url,
-            {
-                "username": "teststaff1",
-                "password": "1234",
-            },
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, 201)
+@pytest.fixture
+def api_client():
+    from rest_framework.test import APIClient
+    return APIClient()
+
+
+@pytest.fixture(scope='session')
+def user_fixture(db):
+    return User.objects.create_user(
+        'testuser', 'testpassword'
+    )
+
+
+def test_superuser_view(admin_client):
+    url = reverse('user-list')
+    response = admin_client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_unauthenticated_user_view(api_client):
+    url = reverse('user-list')
+    response = api_client.get(url)
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+   'username, password, status_code', [
+    #    (None, None, 400),
+    #    (None, 'strong_pass', 400),
+    #    ('user@example.com', None, 400),
+    #    ('user@example.com', 'invalid_pass', 400),
+       ('user@example.com', 'strong_pass', 201),
+   ]
+)
+def test_signup_data_validation(
+   username, password, status_code, api_client
+):
+    url = reverse('user-signup')
+    data = {
+        'username': username,
+        'password': password
+    }
+    response = api_client.post(url, data=data)
+    assert response.status_code == status_code
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+   'username, password, status_code', [
+       ('testuser', 'invalid_pass', 404),
+    #    ('testuser', 'testpassword', 200),
+   ]
+)
+def test_login(
+   username, password, status_code, api_client
+):
+    url = reverse('user-login')
+    data = {
+        'username': username,
+        'password': password
+    }
+    response = api_client.post(url, data=data)
+    assert response.status_code == status_code
--- user/urls.py
+++ user/urls.py
@@ -6,9 +6,9 @@ from user import views
 # router.register(r'user', views.UserList, basename="snippet")
 
 urlpatterns = [
-    path('user/', views.UserList.as_view()),
-    path('user/<int:pk>/', views.UserDetail.as_view()),
-    path("user/signup", views.sign_up_view),
-    path("user/login", views.login_view),
-    path("user/logout", views.logout_view),
+    path('user/', views.UserList.as_view(), name="user-list"),
+    path('user/<int:pk>/', views.UserDetail.as_view(), name="user-detail"),
+    path("user/signup", views.sign_up_view, name="user-signup"),
+    path("user/login", views.login_view, name="user-login"),
+    path("user/logout", views.logout_view, name="user-logout"),
 ]
