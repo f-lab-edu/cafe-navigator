7b3f45deb5adcfc9e1e10319e780b17cbdd36559
유수화 <dbtnghk528@gmail.com>
2023-10-21 07:30:07 +0900

    create .env file in github action & modify requirements

--- cafe_navigator/settings/action.py
+++ cafe_navigator/settings/action.py
@@ -1,5 +1,17 @@
 from .base import *
 
+# def get_env_variable(env_key, default=None):
+#     try:
+#         env_value = os.environ[env_key]
+#         if env_value in ('True', 'False'):
+#             return env_value == 'True'
+#         else:
+#             return env_value
+#     except KeyError:
+#         if default is None:
+#             error_msg = f'Environment variable "{env_key}" does not configured.'
+#             raise ImproperlyConfigured(error_msg)
+#         return default
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
