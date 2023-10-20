20ba0ee7bb3e6627e5b2a465ddc5d5fea0d41091
유수화 <dbtnghk528@gmail.com>
2023-10-21 07:37:40 +0900

    fix db info

--- cafe_navigator/settings/action.py
+++ cafe_navigator/settings/action.py
@@ -15,16 +15,16 @@ from .base import *
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'HOST': get_env_variable('DB_HOST'),
-#         'PORT': get_env_variable('DB_PORT'),
-#         'NAME': get_env_variable('DB_NAME'),
-#         'USER': get_env_variable('DB_USER'),
-#         'PASSWORD': get_env_variable('DB_PASSWORD'),
-#         'TEST': {
-#             'NAME': 'test_db',
-#         },
-#     },
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'NAME': 'test_db',
+        'USER': 'root',
+        'PASSWORD': 'navi',
+        'TEST': {
+            'NAME': 'test_db',
+        },
+    },
+}
