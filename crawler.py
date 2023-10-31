bdc1a505afa02699c4774d8cb5c5a50ebaeb9d1b
유수화 <dbtnghk528@gmail.com>
2023-10-31 23:11:03 +0900

    insert data to mysql

--- crawler.py
+++ crawler.py
@@ -5,6 +5,8 @@ import datetime
 import collections
 import os
 
+from mgdb import MysqlManager
+
 def make_url(keyword, start_x, start_y, end_x, end_y):
     return {
         "url": 'https://dapi.kakao.com/v2/local/search/keyword.json',
@@ -17,21 +19,20 @@ def make_url(keyword, start_x, start_y, end_x, end_y):
     }
 
 
-async def fetch_places(session, requests_condition, all_data_list):
+async def fetch_places(session, requests_condition):
     while True:
         async with session.get(**requests_condition) as response:
             data = await response.text()
             res = json.loads(data)
             search_count = res['meta']['total_count']
             pageable_count = res['meta']['pageable_count']
-            print(search_count, pageable_count)
+            
+            db = MysqlManager()
             if search_count <= 45:
+                db.insert_cafe(res['documents'])
                 if res['meta']['is_end']:
-                    all_data_list.extend(res['documents'])
                     return
-                else:
-                    all_data_list.extend(res['documents'])
-                    requests_condition["params"]["page"] += 1
+                requests_condition["params"]["page"] += 1
             else:
                 tasks = []
                 keyword = requests_condition["params"]["query"]
@@ -40,23 +41,22 @@ async def fetch_places(session, requests_condition, all_data_list):
 
                 dividing_x = (start_x + end_x) / 2
                 dividing_y = (start_y + end_y) / 2
-                t1 = get_places(keyword, start_x, start_y, dividing_x, dividing_y, all_data_list=all_data_list)
-                t2 = get_places(keyword, dividing_x, start_y, end_x, dividing_y, all_data_list=all_data_list)
-                t3 = get_places(keyword, start_x, dividing_y, dividing_x, end_y, all_data_list=all_data_list)
-                t4 = get_places(keyword, dividing_x, dividing_y, end_x, end_y, all_data_list=all_data_list)
+                t1 = await fetch_places(session, make_url(keyword, start_x, start_y, dividing_x, dividing_y))
+                t2 = await fetch_places(session, make_url(keyword, dividing_x, start_y, end_x, dividing_y))
+                t3 = await fetch_places(session, make_url(keyword, start_x, dividing_y, dividing_x, end_y))
+                t4 = await fetch_places(session, make_url(keyword, dividing_x, dividing_y, end_x, end_y))
                 tasks.extend([t1, t2, t3, t4])
                 await asyncio.gather(*tasks)
-                return all_data_list
+                return 
 
-async def get_places(keyword, start_x, start_y, end_x, end_y, all_data_list=[]):
+async def get_places(keyword, start_x, start_y, end_x, end_y):
     requests_condition = make_url(keyword, start_x, start_y, end_x, end_y)
     
     async with aiohttp.ClientSession() as session:
-        await fetch_places(session, requests_condition, all_data_list)
+        await fetch_places(session, requests_condition)
 
 async def overlapped_data(keyword, start_x, start_y, next_x, next_y, num_x, num_y):
     start_time = datetime.datetime.now()
-    overlapped_result = []
 
     tasks = []
     for i in range(1, num_x + 1):
@@ -65,7 +65,7 @@ async def overlapped_data(keyword, start_x, start_y, next_x, next_y, num_x, num_
 
         for j in range(1, num_y + 1):
             end_y = initial_start_y + next_y
-            task = get_places(keyword, start_x, initial_start_y, end_x, end_y, all_data_list=overlapped_result)
+            task = get_places(keyword, start_x, initial_start_y, end_x, end_y)
             tasks.append(task)
             initial_start_y = end_y
         start_x = end_x
@@ -75,7 +75,7 @@ async def overlapped_data(keyword, start_x, start_y, next_x, next_y, num_x, num_
     end_time = datetime.datetime.now()
     print(f"speed: {end_time - start_time}")
 
-    return overlapped_result
+    return 
 
 # 서울 위도 경도
 start_x = 126.8
@@ -88,7 +88,8 @@ num_y = 4
 async def main():
     overlapped_result = await overlapped_data("카페", start_x, start_y, next_x, next_y, num_x, num_y)
     results = list(map(dict, collections.OrderedDict.fromkeys(tuple(sorted(d.items())) for d in overlapped_result)))
-    print(len(results))
+    with open("./cafe_info.json", 'w') as file:
+        json.dump(results, file)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
